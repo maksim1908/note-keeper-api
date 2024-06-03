b2f5c59d08ac54966fd25416ae8ff9e0957718da
Maksim <prorok.p.2016@mail.ru>
2024-06-04 01:39:32 +0300

    add User entity

---
+++ src/main/java/com/example/note_keeper_api/Entities/User.java
@@ -0,0 +1,40 @@
+package com.example.note_keeper_api.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(String password, String username) {
+        this.password = password;
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
