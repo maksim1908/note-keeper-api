83241f487f78990ceae3ca88a33c59fda1127185
Maksim <prorok.p.2016@mail.ru>
2024-06-03 19:14:49 +0300

    delete mapping

--- src/main/java/com/example/note_keeper_api/Controllers/NoteController.java
+++ src/main/java/com/example/note_keeper_api/Controllers/NoteController.java
@@ -13,7 +13,6 @@ public class NoteController {
     public ResponseEntity getAll(){
         return new ResponseEntity<>(new NoteEntity(2,"sdsds","22"),HttpStatus.CREATED);
     }
-    ///////////////
 
     @GetMapping("/notes/{id}")
     public ResponseEntity getById(@PathVariable("id") int id){
