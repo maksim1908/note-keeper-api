58496ef2dab87b9cd9d86f9fe85a6f98c5ddf6b3
Maksim Ivaniuk <100865148+maksim1908@users.noreply.github.com>
2024-06-15 04:21:09 +0300

    Delete src/main/java/com/example/note_keeper_api/Execeptions directory

--- src/main/java/com/example/note_keeper_api/Execeptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.note_keeper_api.Execeptions;
-
-public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/note_keeper_api/Execeptions/UsernameAlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package com.example.note_keeper_api.Execeptions;
-
-public class UsernameAlreadyExistException  extends Exception{
-
-    public UsernameAlreadyExistException(String message){
-        super(message);
-    }
-}
